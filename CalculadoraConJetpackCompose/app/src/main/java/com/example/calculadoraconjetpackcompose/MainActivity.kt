4884a7dc1a7d3cb98eb50cedb6aefbfdb6a1a688
RubenGBar <ruben.garcia@iesnervion.es>
2024-10-21 19:51:27 +0200

    Calculadora con Jetpack Compose

---
+++ CalculadoraConJetpackCompose/app/src/main/java/com/example/calculadoraconjetpackcompose/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.calculadoraconjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "login"
+            ) {
+                composable(route="login") { loginView(navController) }
+                composable("calculadora/{nombre}") { backStackEntry ->
+                    calculadoraView(
+                        navController,
+                        backStackEntry.arguments?.getString("nombre"),
+                    )
+                }
+
+            }
+        }
+    }
+}
