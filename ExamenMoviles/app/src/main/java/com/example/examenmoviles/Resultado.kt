f4b400e5426cd05ddff7ccfd17f466259d22aeac
RubenGBar <ruben.garcia@iesnervion.es>
2024-10-22 11:52:46 +0200

    aaa

--- ExamenMoviles/app/src/main/java/com/example/examenmoviles/Resultado.kt
+++ ExamenMoviles/app/src/main/java/com/example/examenmoviles/Resultado.kt
@@ -23,25 +23,29 @@ fun resultado(
 
     Text("Bienvenido $nombre")
 
-    if(IMC < 18.5){
+    if(IMC != null){
 
-        Text("Su IMC es: $IMC")
-        Text("Usted tiene un peso bajo")
+        if(IMC < 18.5){
 
-    }else if(IMC >= 18.5 && IMC < 24.9){
+            Text("Su IMC es: $IMC")
+            Text("Usted tiene un peso bajo")
 
-        Text("Su IMC es: $IMC")
-        Text("Usted tiene un peso normal")
+        }else if(IMC >= 18.5 && IMC < 24.9){
 
-    }else if(IMC >= 24.9 && IMC < 29.9){
+            Text("Su IMC es: $IMC")
+            Text("Usted tiene un peso normal")
 
-        Text("Su IMC es: $IMC")
-        Text("Usted tiene un peso alto")
+        }else if(IMC >= 24.9 && IMC < 29.9){
 
-    }else if(IMC >= 130 ){
+            Text("Su IMC es: $IMC")
+            Text("Usted tiene un peso alto")
 
-        Text("Su IMC es: $IMC")
-        Text("Usted tiene un peso muy alto")
+        }else if(IMC >= 130 ){
+
+            Text("Su IMC es: $IMC")
+            Text("Usted tiene un peso muy alto")
+
+        }
 
     }
 
