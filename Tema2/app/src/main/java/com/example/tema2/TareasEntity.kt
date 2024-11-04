1db387ad45b2456400f9f0d4957dfaef29d07de5
RubenGBar <ruben.garcia@iesnervion.es>
2024-11-04 12:48:32 +0100

    Nuevo Tema Empezado

---
+++ Tema2/app/src/main/java/com/example/tema2/TareasEntity.kt
@@ -0,0 +1,12 @@
+package com.example.tema2
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task_entity")
+data class TareasEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id:Int = 0, // Id de la tarea
+    var name:String = "", // Nombre de la tarea
+    var isDone:Boolean = false // Booleano que indica si la tarea está hecha o no
+)
